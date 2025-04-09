05caa1e378ee9e6ba0d10544ed63b96474413d1d
Ee Durbin <ewdurbin@gmail.com>
2025-04-09 17:58:57 -0400

    migrate to graphql for recheck all

--- cla/management/commands/recheck_pr.py
+++ cla/management/commands/recheck_pr.py
@@ -1,11 +1,71 @@
 import asyncio
 from collections import namedtuple
 
+import cachetools
+import httpx
+import stamina
 from django.core.management.base import BaseCommand
 from django_github_app.github import AsyncGitHubAPI
 from django_github_app.models import Repository
+from gidgethub import abc as gh_abc
 
 from cla.events import handle_pull_request
+
+cache: cachetools.LRUCache = cachetools.LRUCache(maxsize=500)
+
+ssl_context = httpx.create_ssl_context()
+
+
+class AsyncGitHubAPITimeout(AsyncGitHubAPI):
+
+    def __init__(
+        self,
+        *args,
+        installation=None,
+        installation_id=None,
+        **kwargs,
+    ):
+        if installation is not None and installation_id is not None:
+            raise ValueError("Must use only one of installation or installation_id")
+
+        self.installation = installation
+        self.installation_id = installation_id
+        self.oauth_token = None
+        self._client = httpx.AsyncClient(verify=ssl_context, timeout=30.0)
+        gh_abc.GitHubAPI.__init__(self, *args, cache=cache, **kwargs)
+
+
+GRAPHQL_QUERY = """
+query ($owner: String!, $repo: String!, $cursor: String, $count: Int) {
+  repository(owner: $owner, name: $repo) {
+    pullRequests(states: OPEN, first: $count, after: $cursor) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        number
+        commits(last: 1) {
+          nodes {
+            commit {
+              statusCheckRollup {
+                contexts(first: 100) {
+                  nodes {
+                    ... on StatusContext {
+                      context
+                      state
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+"""
 
 
 class Command(BaseCommand):
@@ -23,22 +83,52 @@ class Command(BaseCommand):
             full_name=options["repo"]
         )
         installation = repository.installation
-        async with AsyncGitHubAPI("clabot", installation=installation) as gh:
+
+        async with AsyncGitHubAPITimeout("clabot", installation=installation) as gh:
             if options["all"]:
-                async for pull in gh.getiter(f"/repos/{repository.full_name}/pulls"):
-                    await handle_pull_request(
-                        namedtuple("Event", "data")(
-                            {
-                                "pull_request": pull,
-                                "repository": {
-                                    "id": repository.repository_id,
-                                    "full_name": repository.full_name,
-                                },
-                            }
-                        ),
-                        gh,
-                    )
-                    print(f"Re-checked {repository.full_name} #{pull['number']}")
+                owner, _, repo = repository.full_name.partition("/")
+                cursor = None
+                while True:
+                    for attempt in stamina.retry_context(on=httpx.HTTPError):
+                        with attempt:
+                            prs = await gh.graphql(
+                                GRAPHQL_QUERY,
+                                checkName="CLA Signing",
+                                owner=owner,
+                                repo=repo,
+                                cursor=cursor,
+                                count=50,
+                            )
+                    for pr in prs["repository"]["pullRequests"]["nodes"]:
+                        number = pr["number"]
+                        contexts = pr["commits"]["nodes"][0]["commit"]["statusCheckRollup"][
+                            "contexts"
+                        ]["nodes"]
+                        status_checks = [
+                            context
+                            for context in contexts
+                            if context.get("context", None) == "CLA Signing"
+                        ]
+                        if status_checks and status_checks[0]["state"] == "FAILURE":
+                            pull = await gh.getitem(
+                                f"/repos/{repository.full_name}/pulls/{number}"
+                            )
+                            await handle_pull_request(
+                                namedtuple("Event", "data")(
+                                    {
+                                        "pull_request": pull,
+                                        "repository": {
+                                            "id": repository.repository_id,
+                                            "full_name": repository.full_name,
+                                        },
+                                    }
+                                ),
+                                gh,
+                            )
+                            print(f"Re-checked {repository.full_name} #{pull['number']}")
+                    cursor = prs["repository"]["pullRequests"]["pageInfo"]["endCursor"]
+                    if not prs["repository"]["pullRequests"]["pageInfo"]["hasNextPage"]:
+                        break
             else:
                 pull = await gh.getitem(f"/repos/{repository.full_name}/pulls/{options['pr']}")
                 await handle_pull_request(
