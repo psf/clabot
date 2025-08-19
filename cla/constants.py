01de44bbd44cae79865f8074e36ccdff51ca6049
Stan Ulbrych <stanulbrych@gmail.com>
2025-08-19 10:21:49 +0200

    Improve not signed badge

--- cla/constants.py
+++ cla/constants.py
@@ -1,6 +1,6 @@
 SIGNED_BADGE = "https://img.shields.io/badge/CLA%20Signed-FAE085?style=flat-square&logo=Python"
 NOT_SIGNED_BADGE = (
     "https://img.shields.io/badge/"
-    "CLA%20Not%20Signed-fa858b?style=flat-square&logo=Python&logoColor=ffffff"
+    "CLA Not Signed — Click To Sign-fa858b?style=flat-square&logo=Python&logoColor=ffffff"
 )
 SENTINEL_MARKER = "<!-- CLA BOT SIGNING COMMENT DO NOT EDIT -->"
